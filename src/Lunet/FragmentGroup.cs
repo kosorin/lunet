a2d25de3f102f63f8b520fb3804b98ca3d8df386
David Kosorin <david@kosorin.net>
2019-12-30 09:08:05 +0100

    Move Fragment class to new file

--- src/Lunet/FragmentGroup.cs
+++ src/Lunet/FragmentGroup.cs
@@ -1,6 +1,5 @@
 ﻿using Lunet.Common;
 using Lunet.Data;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -70,27 +69,6 @@ namespace Lunet
             base.Dispose(disposing);
 
             _disposed = true;
-        }
-    }
-
-    internal class Fragment : PoolableObject<Fragment>
-    {
-        private readonly byte[] _data = new byte[ushort.MaxValue];
-        private int _length;
-
-        public byte Index { get; private set; }
-
-        public void Set(byte index, ReadOnlySpan<byte> data)
-        {
-            Index = index;
-
-            _length = data.Length;
-            data.CopyTo(new Span<byte>(_data, 0, _length));
-        }
-
-        public void WriteTo(NetDataWriter writer)
-        {
-            writer.WriteSpan(new ReadOnlySpan<byte>(_data, 0, _length));
         }
     }
 }
