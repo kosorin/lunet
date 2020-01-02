1d606ca7ec7aac5135a524adf5585d8402f42c54
David Kosorin <david@kosorin.net>
2020-01-02 17:20:03 +0100

    Test benchmark

--- tests/Lunet.Benchmarks/Program.cs
+++ tests/Lunet.Benchmarks/Program.cs
@@ -1,4 +1,9 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Running;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Lunet.Benchmarks
 {
@@ -7,6 +12,54 @@ namespace Lunet.Benchmarks
         private static void Main(string[] args)
         {
             BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        }
+    }
+
+    [Config(typeof(Config))]
+    public class ProgramBenchmarks
+    {
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                Add(MemoryDiagnoser.Default);
+            }
+        }
+
+
+        private List<int> _data;
+
+
+        [Params(10, 100, 1000)]
+        public int Count;
+
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+        }
+
+        [IterationSetup]
+        public void IterationSetup()
+        {
+            _data = Enumerable.Repeat(1, Count).Select((x, i) => x * i).ToList();
+        }
+
+        [IterationCleanup]
+        public void IterationCleanup()
+        {
+        }
+
+
+        [Benchmark]
+        public List<int> Benchmark()
+        {
+            return _data.Select(x => x + 1).ToList();
         }
     }
 }
